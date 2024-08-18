ac82c4400e122a8ba1cbb612b3a2f9cf142c5682
youvegotnigel <nigeldave19@gmail.com>
2024-08-18 15:49:06 +0530

    Update SampleTests.cs

--- SampleTests.cs
+++ SampleTests.cs
@@ -1,15 +1,32 @@
-﻿namespace AtataAdvancedNunitProject
+﻿using AtataAdvancedNunitProject.Components;
+
+namespace AtataAdvancedNunitProject
 {
     public sealed class SampleTests : UITestFixture
     {
         [Test]
         public void VerifyPageTitle()
         {
-            Go.To<OrdinaryPage>()
-                    .PageTitle.Should.Contain("Atata");
+            Go.To<OrdinaryPage>().PageTitle.Should.Contain("Atata");
+            AtataContext.Current.TakeScreenshot();
+        }
+
+
+        [Test]
+        public void VerifySignIn()
+        {
+            Go.To<SignInPage>().PageTitle.Should.Contain("Sign In");
+
+            Go.To<SignInPage>()
+            .Email.Type("admin@mail.com")
+            .Password.Type("abc123")
+            .SignIn.Click();
+
 
             AtataContext.Current.TakeScreenshot();
         }
+
+
 
     }
 }
