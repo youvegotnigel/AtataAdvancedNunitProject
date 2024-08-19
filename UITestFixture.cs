db4c896986e6f9240009b9b564aeaff88e7a8d85
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 23:13:30 +0530

    Update UITestFixture.cs

--- UITestFixture.cs
+++ UITestFixture.cs
@@ -1,8 +1,17 @@
-﻿namespace AtataAdvancedNunitProject
+﻿using AtataAdvancedNunitProject.Components;
+
+namespace AtataAdvancedNunitProject
 {
+
+    [TestFixture]
     [Parallelizable(ParallelScope.Self)]
     public class UITestFixture
     {
+
+        public static AtataConfig Config =>
+            AtataConfig.Current;
+
+
         [SetUp]
         public void SetUp() =>
             AtataContext.Configure().Build();
@@ -10,5 +19,12 @@
         [TearDown]
         public void TearDown() =>
             AtataContext.Current?.Dispose();
+
+        protected static UsersPage Login() =>
+            Go.To<SignInPage>()
+                .Email.Set(Config.Account.Email)
+                .Password.Set(Config.Account.Password)
+                .SignIn();
+
     }
 }
