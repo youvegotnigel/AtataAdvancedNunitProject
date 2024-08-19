0131170d08601b90fc7c715c8c9d3713d2d21e97
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 21:40:03 +0530

    Create UsersPage.cs

---
+++ Components/UsersPage.cs
@@ -0,0 +1,34 @@
+﻿namespace AtataAdvancedNunitProject.Components
+{
+    using _ = UsersPage;
+
+    [Url("users")]
+    public class UsersPage : BasePage<_>
+    {
+
+        public ButtonDelegate<UserEditWindow, _> New { get; private set; }
+        public Table<UserTableRow, _> Users { get; private set; }
+
+        public UserTableRow GetUserRow(string email) =>
+            Users.Rows[x => x.Email == email];
+
+        public class UserTableRow : TableRow<_>
+        {
+            public Text<_> FirstName { get; private set; }
+
+            public Text<_> LastName { get; private set; }
+
+            public Text<_> Email { get; private set; }
+
+            public Content<Office, _> Office { get; private set; }
+
+            public LinkDelegate<UserDetailsPage, _> View { get; private set; }
+
+            public ButtonDelegate<UserEditWindow, _> Edit { get; private set; }
+
+            [CloseConfirmBox]
+            public ButtonDelegate<_> Delete { get; private set; }
+        }
+
+    }
+}
