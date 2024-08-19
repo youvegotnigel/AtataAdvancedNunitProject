357cc1e72a068ae7cff86de2795d686a8f134687
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 21:39:59 +0530

    Create ValidationMessage.cs

---
+++ Controls/ValidationMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace AtataAdvancedNunitProject.Controls
+{
+    [ControlDefinition("div[contains(concat(' ', normalize-space(@class), ' '), ' has-error ')]//span[contains(concat(' ', normalize-space(@class), ' '), ' help-block ')]")]
+    public class ValidationMessage<TOwner> : Text<TOwner>
+    where TOwner : PageObject<TOwner>
+    {
+        public new FieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> Should =>
+            new(this);
+    }
+}
