4059d8816bb7c9a706d62a676aabe87463bec85b
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 22:59:01 +0530

    Update UserEditWindow.cs

--- Components/UserEditWindow.cs
+++ Components/UserEditWindow.cs
@@ -1,44 +1,7 @@
 ﻿namespace AtataAdvancedNunitProject.Components
 {
-
-    using _ = UserEditWindow;
-
-    internal class UserEditWindow
+    public class UserEditWindow
     {
-        public GeneralTabPane General { get; private set; }
-
-        public AdditionalTabPane Additional { get; private set; }
-
-        [Term("Save", "Create")]
-        public ButtonDelegate<UsersPage, _> Save { get; private set; }
-
-        public ButtonDelegate<UsersPage, _> Cancel { get; private set; }
-
-        [FindByClass]
-        public ButtonDelegate<UsersPage, _> Close { get; private set; }
-
-        public Controls.ValidationMessageList<_> ValidationMessages { get; private set; }
-
-        public class GeneralTabPane : BSTabPane<_>
-        {
-            public TextInput<_> FirstName { get; private set; }
-
-            public TextInput<_> LastName { get; private set; }
-
-            [RandomizeStringSettings("{0}@mail.com")]
-            public TextInput<_> Email { get; private set; }
-
-            public Select<Office?, _> Office { get; private set; }
-
-            [FindByName]
-            public RadioButtonList<Gender?, _> Gender { get; private set; }
-        }
-
-        public class AdditionalTabPane : BSTabPane<_>
-        {
-            public DateInput<_> Birthday { get; private set; }
-
-            public TextArea<_> Notes { get; private set; }
-        }
+        //https://github.com/atata-framework/atata-sample-app-tests/blob/main/test/AtataSampleApp.UITests.Components/UserEditWindow.cs
     }
 }
