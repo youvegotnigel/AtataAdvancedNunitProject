83802f3f83ba27ca99e1ddc8bf2c48a9f10369b0
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 22:48:54 +0530

    Update AtataConfig.cs

--- AtataConfig.cs
+++ AtataConfig.cs
@@ -1,8 +1,22 @@
 ﻿namespace AtataAdvancedNunitProject
 {
+
+    /// <summary>
+    /// Represents configuration that is read from "Atata.json".
+    /// The name of class doesn't matter, it can be: "Config", "AppConfig", etc.
+    /// </summary>
     public sealed class AtataConfig : JsonConfig<AtataConfig>
     {
         // Custom configuration properties can be added here.
         // See https://github.com/atata-framework/atata-configuration-json#custom-settings
+
+        public AccountConfiguration Account { get; set; }
+
+        public class AccountConfiguration
+        {
+            public string Email { get; set; }
+
+            public string Password { get; set; }
+        }
     }
 }
