5bcdfa54a4cb0a38f01a7466b4faf33c31a947e1
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 22:58:10 +0530

    Update UsersPage.cs

--- Components/UsersPage.cs
+++ Components/UsersPage.cs
@@ -6,7 +6,7 @@
     public class UsersPage : BasePage<_>
     {
 
-        public ButtonDelegate<UserEditWindow, _> New { get; private set; }
+        //public ButtonDelegate<UserEditWindow, _> New { get; private set; }
         public Table<UserTableRow, _> Users { get; private set; }
 
         public UserTableRow GetUserRow(string email) =>
@@ -20,11 +20,11 @@
 
             public Text<_> Email { get; private set; }
 
-            public Content<Office, _> Office { get; private set; }
+            //public Content<Office, _> Office { get; private set; }
 
-            public LinkDelegate<UserDetailsPage, _> View { get; private set; }
+            //public LinkDelegate<UserDetailsPage, _> View { get; private set; }
 
-            public ButtonDelegate<UserEditWindow, _> Edit { get; private set; }
+            //public ButtonDelegate<UserEditWindow, _> Edit { get; private set; }
 
             [CloseConfirmBox]
             public ButtonDelegate<_> Delete { get; private set; }
