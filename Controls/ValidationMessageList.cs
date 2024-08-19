3b38ef9d0ce380941efc5e8946ffd36e6f201870
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 21:39:56 +0530

    Create ValidationMessageList.cs

---
+++ Controls/ValidationMessageList.cs
@@ -0,0 +1,24 @@
+﻿namespace AtataAdvancedNunitProject.Controls
+{
+
+    public class ValidationMessageList<TOwner> : ControlList<ValidationMessage<TOwner>, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        public ValidationMessage<TOwner> this[Func<TOwner, IControl<TOwner>> controlSelector] =>
+        For(controlSelector);
+
+        public ValidationMessage<TOwner> For(Func<TOwner, IControl<TOwner>> controlSelector)
+        {
+            var validationMessageDefinition = UIComponentResolver.GetControlDefinition(typeof(ValidationMessage<TOwner>));
+
+            IControl<TOwner> boundControl = controlSelector(Component.Owner);
+
+            PlainScopeLocator scopeLocator = new PlainScopeLocator(By.XPath("ancestor::" + validationMessageDefinition.ScopeXPath))
+            {
+                SearchContext = boundControl.Scope
+            };
+
+            return Component.Controls.Create<ValidationMessage<TOwner>>(boundControl.ComponentName, scopeLocator);
+        }
+    }
+}
