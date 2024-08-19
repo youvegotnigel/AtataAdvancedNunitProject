685063a06ad7c3bb460137ac641b33640d8d93ec
youvegotnigel <nigeldave19@gmail.com>
2024-08-19 23:09:37 +0530

    temp comment

--- SignInTests.cs
+++ SignInTests.cs
@@ -1,6 +1,4 @@
-﻿using AtataAdvancedNunitProject.Components;
-
-namespace AtataAdvancedNunitProject
+﻿namespace AtataAdvancedNunitProject
 {
     public class SignInTests : UITestFixture
     {
@@ -8,24 +6,27 @@ namespace AtataAdvancedNunitProject
         public void SignIn() =>
             Login();
 
-        [Test]
-        public void Validation_Required() =>
-            Go.To<SignInPage>()
-                .SignIn.Click()
-                .ValidationMessages[x => x.Email].Should.BeRequired()
-                .ValidationMessages[x => x.Password].Should.BeRequired()
-                .Email.Set(Config.Account.Email)
-                .Password.Set(Config.Account.Password)
-                .SignIn();
+        //[Test]
+        //public void Validation_Required() =>
+        //    Go.To<SignInPage>()
+        //        .SignIn.Click()
+        //        .ValidationMessages[x => x.Email].Should.BeRequired()
+        //        .ValidationMessages[x => x.Password].Should.BeRequired()
+        //        .Email.Set(Config.Account.Email)
+        //        .Password.Set(Config.Account.Password)
+        //        .SignIn();
 
-        [Test]
-        public void Validation_InvalidPassword() =>
-            Go.To<SignInPage>()
-                .Email.Set(Config.Account.Email)
-                .Password.Set(Config.Account.Password + "!")
-                .SignIn.Click()
-                .ValidationMessages.Should.Contain(TermMatch.Contains, "invalid")
-                .Password.Set(Config.Account.Password)
-                .SignIn();
+
+
+        //[Test]
+        //public void Validation_InvalidPassword() =>
+        //    Go.To<SignInPage>()
+        //        .Email.Set(Config.Account.Email)
+        //        .Password.Set(Config.Account.Password + "!")
+        //        .SignIn.Click()
+        //        .ValidationMessages.Should.Contain(TermMatch.Contains, "invalid")
+        //        .Password.Set(Config.Account.Password)
+        //        .SignIn();
+
     }
 }
