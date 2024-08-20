5f5af6a5f9c77a20528c896487b3a9a2bd560f47
youvegotnigel <nigeldave19@gmail.com>
2024-08-20 23:30:12 +0530

    Create BaseTests.cs

---
+++ BaseTests.cs
@@ -0,0 +1,32 @@
+﻿using AtataAdvancedNunitProject.Components;
+
+namespace AtataAdvancedNunitProject
+{
+    public sealed class BaseTests : UITestFixture
+    {
+        [Test]
+        public void VerifyPageTitle()
+        {
+            Go.To<OrdinaryPage>().PageTitle.Should.Contain("Atata");
+            AtataContext.Current.TakeScreenshot();
+        }
+
+
+        [Test]
+        public void VerifySignIn()
+        {
+            Go.To<SignInPage>().PageTitle.Should.Contain("Sign In");
+
+            Go.To<SignInPage>()
+            .Email.Type("admin@mail.com")
+            .Password.Type("abc123")
+            .SignIn.Click();
+
+
+            AtataContext.Current.TakeScreenshot();
+        }
+
+
+
+    }
+}
