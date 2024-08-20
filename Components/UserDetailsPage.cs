429808765cbb11f7e6b8c2e1ede5e6521214f17f
youvegotnigel <nigeldave19@gmail.com>
2024-08-20 23:30:14 +0530

    Create UserDetailsPage.cs

---
+++ Components/UserDetailsPage.cs
@@ -0,0 +1,21 @@
+﻿namespace AtataAdvancedNunitProject.Components
+{
+    using _ = UserDetailsPage;
+    public sealed class UserDetailsPage : Page<_>
+    {
+        [FindByDescriptionTerm]
+        public Text<_> Email { get; private set; }
+
+        //[FindByDescriptionTerm]
+        //public Content<Office, _> Office { get; private set; }
+
+        //[FindByDescriptionTerm]
+        //public Content<Gender, _> Gender { get; private set; }
+
+        [FindByDescriptionTerm]
+        public Content<DateTime?, _> Birthday { get; private set; }
+
+        [FindByDescriptionTerm]
+        public Text<_> Notes { get; private set; }
+    }
+}
