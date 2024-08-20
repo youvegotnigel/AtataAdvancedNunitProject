2b5cb64b7bfb53b17aa7a4053c62a71dc08653e6
youvegotnigel <nigeldave19@gmail.com>
2024-08-20 23:09:44 +0530

    Create IValidationMessageVerificationProviderExtensions.cs

---
+++ Extensions/IValidationMessageVerificationProviderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AtataAdvancedNunitProject.Controls;
+
+namespace AtataAdvancedNunitProject.Extensions;
+public static class IValidationMessageVerificationProviderExtensions
+{
+    public static TOwner BeRequired<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should)
+        where TOwner : PageObject<TOwner>
+        =>
+        should.Equal("is required");
+
+    public static TOwner HaveIncorrectFormat<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should)
+        where TOwner : PageObject<TOwner>
+        =>
+        should.Equal("has incorrect format");
+
+    public static TOwner HaveMinLength<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should, int length)
+        where TOwner : PageObject<TOwner>
+        =>
+        should.Equal($"minimum length is {length}");
+
+    public static TOwner HaveMaxLength<TOwner>(this IFieldVerificationProvider<string, ValidationMessage<TOwner>, TOwner> should, int length)
+        where TOwner : PageObject<TOwner>
+        =>
+        should.Equal($"maximum length is {length}");
+}
