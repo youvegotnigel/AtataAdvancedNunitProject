30a437bb4204bd2a6cdff3dde275b1c000aa76c7
youvegotnigel <nigeldave19@gmail.com>
2024-08-21 13:47:51 +0530

    Update BasePage.cs

--- Components/BasePage.cs
+++ Components/BasePage.cs
@@ -1,5 +1,8 @@
-﻿namespace AtataAdvancedNunitProject.Components;
+﻿using AtataAdvancedNunitProject.Controls;
 
+namespace AtataAdvancedNunitProject.Components;
+
+[WaitForDocumentReadyState(Priority = TriggerPriority.Highest)]
 public abstract class BasePage<TOwner> : Page<TOwner>
     where TOwner : BasePage<TOwner>
 {
@@ -17,4 +20,9 @@ public abstract class BasePage<TOwner> : Page<TOwner>
 
     [FindByContent("Sign Up")]
     public Link<SignUpPage, TOwner> SignUpPage { get; private set; }
+
+    public MainMenu<TOwner> Menu { get; private set; }
+
+    [FindFirst]
+    public H1<TOwner> Header { get; private set; }
 }
