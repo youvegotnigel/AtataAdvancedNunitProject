1f1f3a7e166a7716911aacb273c8793488ec7aff
youvegotnigel <nigeldave19@gmail.com>
2024-08-21 13:47:48 +0530

    Update UserDetailsPage.cs

--- Components/UserDetailsPage.cs
+++ Components/UserDetailsPage.cs
@@ -1,16 +1,17 @@
 ﻿namespace AtataAdvancedNunitProject.Components
 {
+    using AtataAdvancedNunitProject.Models;
     using _ = UserDetailsPage;
-    public sealed class UserDetailsPage : Page<_>
+    public sealed class UserDetailsPage : BasePage<_>
     {
         [FindByDescriptionTerm]
         public Text<_> Email { get; private set; }
 
-        //[FindByDescriptionTerm]
-        //public Content<Office, _> Office { get; private set; }
+        [FindByDescriptionTerm]
+        public Content<Office, _> Office { get; private set; }
 
-        //[FindByDescriptionTerm]
-        //public Content<Gender, _> Gender { get; private set; }
+        [FindByDescriptionTerm]
+        public Content<Gender, _> Gender { get; private set; }
 
         [FindByDescriptionTerm]
         public Content<DateTime?, _> Birthday { get; private set; }
